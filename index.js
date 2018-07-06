67e44c7fad53c660f8349f48c33e1154d840a1c5
Jo Lee <jo.lee@softwire.com>
2018-07-06 09:50:04 +0100

    pretty printing data and calculate timeToBus

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const moment = require('moment');
 
 let stopID = '490008660N';
 
-
+console.log(`please enter a stopID: `);
 process.stdin.on('readable', () => {
+
     let input = process.stdin.read().toString().slice(0, -1); // remove \n
-    input = '490008660N'; //todo: remove once done testing
+    if(input.toLowerCase() === "q"){
+        process.exit(0);
+    }
+    //input = '490008660N'; //todo: remove once done testing
     let url = `https://api.tfl.gov.uk/StopPoint/${input}/Arrivals?app_id=${keys.appID}&app_key=${keys.appKey}`;
     request(url, (err, response, body) => {
         if(err) {
@@ -21,9 +25,20 @@ process.stdin.on('readable', () => {
         // console.log(JSON.parse(body).length);
         let nextBuses = JSON.parse(body);
         nextBuses.splice(5);
-        console.log(nextBuses.length);
+        //console.log(nextBuses);
         //console.log('body:', body);
+        nextBuses = nextBuses.map(bus => ({
+            route: bus.lineId,
+            dest: bus.destinationName,
+            ETA: moment(bus.expectedArrival)
+        }));
+        nextBuses.sort((b1, b2) => b1.ETA - b2.ETA);
+        const now = moment();
+        nextBuses.forEach(bus => {
+            let timeToBus = Math.floor((bus.ETA - now) / 1000 / 60);
+            console.log(`Bus number: ${bus.route}\nTowards: ${bus.dest}\nETA: ${timeToBus}min\n`);
+        });
+
+        console.log("please enter another stopID");
     });
 });
-
-// https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals?app_id=93d97c7c&app_key=c7263864d367c5d5138bf2d0e27d9704
