4f3e3c32292acbbafa7ea9a931818cb8dc9a8bac
Jo Lee <jo.lee@softwire.com>
2018-07-09 11:03:32 +0100

    create departure boards endpoint for server

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 const tflArrivals = require("./tflapi/arrivals");
 const post = require("./postcodeapi/latlon");
 const radius = require("./tflapi/radius");
-const rl = require('readline-sync');
+const express = require('express');
 
-let print = stops => stops.forEach(stop => {
-    console.log(`Stop \"${stop.stop.name}\" ${Math.round(stop.stop.distance)}m away`);
-    stop.buses.forEach(bus => {
-        console.log(`- Bus ${bus.route}\n  Towards: ${bus.destination}\n  ETA: ${bus.timeToStop}min\n`);
-    })
+const app = express();
+app.get('/departureBoards', (req, res) => {
+    Promise.resolve(req.query.postcode)
+        .then(post)
+        .then(radius)
+        .then(stops => Promise.all(stops.map(tflArrivals)))
+        .then(str => res.send(str))
+        .catch(e => {
+            console.warn(e);
+            res.send(e.message);
+        });
 });
 
-let input = rl.question("Enter a postcode or \"q\" to cancel\n");
-Promise.resolve(input)
-    .then(input => input === "q" ? process.exit(0) : input)
-    .then(post)
-    .then(radius)
-    .then(stops => Promise.all(stops.map(tflArrivals)))
-    .then(print)
-    .catch(e => console.warn(e));
+app.listen(3000);
