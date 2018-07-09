b3f577d8c927a01a6e48b99bdae7f69204f24cc1
Jo Lee <jo.lee@softwire.com>
2018-07-09 12:17:12 +0100

    increased browser support

--- frontend/departures.js
+++ frontend/departures.js
@@ -1,23 +1,23 @@
-let getBuses = (param) => {
-    let xhttp = new XMLHttpRequest();
+function getBuses(param) {
+    var xhttp = new XMLHttpRequest();
 
     xhttp.open('GET', `http://localhost:3000/departureBoards?postcode=${param.elements[0].value}`, true);
     xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.onload = () => {
-        if(xhttp.status === 200) {
-            let response = JSON.parse(xhttp.responseText);
-            let html;
-            if(response.length === 0) {
+    xhttp.onload = function () {
+        if (xhttp.status === 200) {
+            var response = JSON.parse(xhttp.responseText);
+            var html;
+            if (response.length === 0) {
                 html = "<h2>No buses found nearby</h2>Is the postcode in London?";
             } else {
                 html = "<h2>Results</h2>";
-                response.forEach(stop => {
+                response.forEach(function (stop) {
                     html += `<h3>${stop.stop.name}</h3>`;
-                    if(stop.buses.length === 0) {
+                    if (stop.buses.length === 0) {
                         html += "No buses found &#128542";
                     } else {
                         html += "<ul>";
-                        stop.buses.forEach(bus => {
+                        stop.buses.forEach(function (bus) {
                             html += `<li>${bus.timeToStop} minute${bus.timeToStop === 1 ? "" : "s"}: ${bus.route} to ${bus.destination}</li>`
                         });
                         html += "</ul>";
